f42a67835adaa0a1bad0278534904a30163b188f
UltraLion <49597041+UltraLionfr@users.noreply.github.com>
2022-09-09 16:15:42 +0200

    Create nodejs18x.sh

---
+++ nodejs18x.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+clear
+VERT="\\033[1;32m" 
+NORMAL="\\033[0;39m" 
+ROUGE="\\033[1;31m" 
+ROSE="\\033[1;35m" 
+BLEU="\\033[1;34m" 
+BLANC="\\033[0;02m" 
+BLANCLAIR="\\033[1;08m" 
+JAUNE="\\033[1;33m" 
+CYAN="\\033[1;36m"
+VERT="\\033[1;32m"
+NORMAL="\\033[0;39m"
+ROUGE="\\033[1;31m"
+ROSE="\\033[1;35m"
+BLEU="\\033[1;34m"
+BLANC="\\033[0;02m"
+BLANCLAIR="\\033[1;08m"
+JAUNE="\\033[1;33m"
+CYAN="\\033[1;36m"
+echo -e "$CYAN""Script d'installion de NodeJsX18 (inclue npm et pm2)" "$BLANC"
+
+cd ~
+curl -sL https://deb.nodesource.com/setup_18.x | sudo bash -
+sudo apt install nodejs -y
+sudo npm i pm2 -g
+node -v
+npm -v
+pm2 -v
